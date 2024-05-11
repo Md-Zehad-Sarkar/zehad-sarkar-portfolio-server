e91c6bdfe68ae22c5d5672be0b3916b0b6a6d254
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-11 13:32:37 +0600

    get all blogs api success with pagination

--- index.js
+++ index.js
@@ -216,6 +216,44 @@ async function run() {
         });
       }
     });
+
+    // ..............................................Get Blogs Api..................................
+    app.get("/api/v1/blogs", async (req, res) => {
+      try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const blogs = await blogsCollection
+          .find()
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+        if (blogs?.length > 0) {
+          res.status(200).json({
+            success: true,
+            message: "All Blogs Retrieved Successful",
+            data: blogs,
+            page: page,
+            limit: limit,
+          });
+        } else {
+          res.status(500).json({
+            success: true,
+            message: "All Blogs Retrieved Successful",
+            data: [],
+            page: 0,
+            limit: 0,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: true,
+          message: "Internal Server Error",
+        });
+      }
+    });
   } finally {
     // await client.close();
   }
