024c7b05f1c8d9144ff4d78341df4cda81335078
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-12 14:19:56 +0600

    courses get api success with pagination

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ async function run() {
         });
       }
     });
-    
+
     // ..............................................Add Course Api..................................
     app.post("/api/v1/add-course", async (req, res) => {
       try {
@@ -379,6 +379,41 @@ async function run() {
           res.status(500).json({
             success: false,
             message: "Course Added Failed",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: false,
+          message: "Something Went Wrong",
+        });
+      }
+    });
+
+    // ..............................................Get Courses Api..................................
+    app.get("/api/v1/courses", async (req, res) => {
+      try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        const courses = await courseCollection
+          .find()
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+
+        if (courses?.length > 0) {
+          res.status(200).json({
+            success: true,
+            message: "Courses Retrieved Successful",
+            data: courses,
+            page: page,
+            limit: limit,
+          });
+        } else {
+          res.status(404).json({
+            success: false,
+            message: "Something Went Wrong",
           });
         }
       } catch (error) {
